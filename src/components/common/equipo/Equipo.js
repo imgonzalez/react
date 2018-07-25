d2575d48f8b9a29990ab781f30a10d4053e17b02
Unknown <imgonzalez@soporte-imx.com>
2018-07-24 20:54:58 -0500

    Star

--- src/components/common/equipo/Equipo.js
+++ src/components/common/equipo/Equipo.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 // Estilos
 import './Equipo.css';
+import Icon  from '@material-ui/core/Icon';
 
 class Equipo extends Component {
   render() {
     return (
       <div>
-        <img className="team-logo" src={this.props.logo} alt={this.props.nombre} />
+        <img className={"team-logo"} src={this.props.logo} alt={this.props.nombre} />
+        <Icon>star_rate</Icon>
       </div>
     );
   }
